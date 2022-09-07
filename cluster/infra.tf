89de2f333f4f6d53dfbdf5a946d596e42e13be3b
Marcelo Oliveira <marcelo@marceloweb.info>
2022-09-07 18:50:39 -0300

    - configs/terraform

--- cluster/infra.tf
+++ cluster/infra.tf
@@ -7,6 +7,15 @@ terraform {
   }
 }
 
+# Set the variable value in *.tfvars file
+# or using -var="do_token=..." CLI option
+variable "do_token" {}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
+}
+
 resource "digitalocean_kubernetes_cluster" "mylab" {
   name   = "mylab"
   region = "sfo3"

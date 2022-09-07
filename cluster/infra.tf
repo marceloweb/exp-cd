2fb34bd94d335a0373704f49f68766e4fffe8162
Marcelo Oliveira <marcelo@marceloweb.info>
2022-09-07 18:40:40 -0300

    - provider

--- cluster/infra.tf
+++ cluster/infra.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
 resource "digitalocean_kubernetes_cluster" "mylab" {
   name   = "mylab"
   region = "sfo3"

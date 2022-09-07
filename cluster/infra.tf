5830871ae0709e4f003071bb7af42c2e2de9ee37
Marcelo Oliveira <marcelo@marceloweb.info>
2022-09-07 18:30:07 -0300

    - resources/digitalocean

--- cluster/infra.tf
+++ cluster/infra.tf
@@ -1,2 +1,19 @@
+resource "digitalocean_kubernetes_cluster" "mylab" {
+  name   = "mylab"
+  region = "sfo3"
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.24.4-do.0"
 
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-1vcpu-2gb"
+    node_count = 3
+
+    taint {
+      key    = "workloadKind"
+      value  = "apps"
+      effect = "NoSchedule"
+    }
+  }
+}
 

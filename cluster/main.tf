8386c8c557e398b70fc9f6a5f59ceb1dff7b9588
Marcelo Oliveira <marcelo@marceloweb.info>
2022-09-07 21:49:55 -0300

    - new variable

--- cluster/main.tf
+++ cluster/main.tf
@@ -7,10 +7,6 @@ terraform {
   }
 }
 
-# Set the variable value in *.tfvars file
-# or using -var="do_token=..." CLI option
-variable "do_token" {}
-
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
   token = var.do_token
